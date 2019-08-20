2c4fe04fe8d32e91181727f4fd3e6ede68965a0e
pshubham95 <pshubham@gmail.com>
2019-08-19 18:03:53 -0700

    Updates

---
+++ precache-manifest.4f1537a50fec8c91d99d678246a196a4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab6652702bf35b5a3e75563b8feea44c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "562b9eb0ccf9c4b7f181",
+    "url": "/static/css/2.61b8705f.chunk.css"
+  },
+  {
+    "revision": "397fc89abf6d6a4d3d50",
+    "url": "/static/css/main.7fa73090.chunk.css"
+  },
+  {
+    "revision": "562b9eb0ccf9c4b7f181",
+    "url": "/static/js/2.b566841e.chunk.js"
+  },
+  {
+    "revision": "397fc89abf6d6a4d3d50",
+    "url": "/static/js/main.7de17cc8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "86e051fcbc13f022d1db1de9487d91bd",
+    "url": "/static/media/asu_sun.86e051fc.png"
+  },
+  {
+    "revision": "af7e1e5d791f9ff0de7a7e9ff5156905",
+    "url": "/static/media/coep.af7e1e5d.jpg"
+  },
+  {
+    "revision": "00681da7e3d0cefe8d00accdd82e600a",
+    "url": "/static/media/facebook.00681da7.svg"
+  },
+  {
+    "revision": "2b1b90735594a680c936d2b2da6bfa0f",
+    "url": "/static/media/ge_aviation.2b1b9073.png"
+  },
+  {
+    "revision": "3364258c579ba84e5f58ddd1f755dbbc",
+    "url": "/static/media/ge_digital.3364258c.png"
+  },
+  {
+    "revision": "d8450c0bad86ec78f812266e8af81e68",
+    "url": "/static/media/iconmonstr-location-1.d8450c0b.svg"
+  },
+  {
+    "revision": "5b238434e2862c877f08572b96c0ef7b",
+    "url": "/static/media/linkedin.5b238434.svg"
+  },
+  {
+    "revision": "c7ea62d541151ed399b8a2d0e13f2a89",
+    "url": "/static/media/profile.c7ea62d5.jpeg"
+  }
+]);
--- src/utils/index.js
+++
@@ -1,3 +0,0 @@
-export default function camelToNormal(str) {
-  return str.replace(/([A-Z]+)/g, ' $1').replace(/([A-Z][a-z])/g, ' $1');
-}
